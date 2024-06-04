188aaf2f9c68432b805957c787ab03bcb03dc792
“talatkuyuk” <talatkuyuk@gmail.com>
2024-06-04 16:40:11 +0300

    added home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,37 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import VisitGithub from "@/components/VisitGithub";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main style={{ display: "grid", justifyItems: "center" }}>
+      <p style={{ marginBottom: "0", textAlign: "center" }}>
+        This is a <strong>blog application</strong> using{" "}
+        <strong>next-mdx-remote-client</strong> in{" "}
+        <code>Next.js pages router</code>.
+      </p>
+      <VisitGithub style={{ marginTop: "0" }} />
+      <div className="next-mdx-remote-client">next-mdx-remote-client</div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
+      <p style={{ textAlign: "center" }}>
+        It is a wrapper of the <code>@mdx-js/mdx</code> in order to load MDX
+        content.
+      </p>
+      <p style={{ textAlign: "center" }}>
+        You can reach the package on{" "}
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          href="https://www.npmjs.com/package/next-mdx-remote-client"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
+          npm
+        </a>{" "}
+        or the repository on{" "}
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          href="https://github.com/ipikuka/next-mdx-remote-client"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          github
         </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        .
+      </p>
     </main>
   );
 }
